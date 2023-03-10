be272b96c58684d1e0a8d6fb074f1f8eafdd0397
paulo <paulo@paulo>
2023-03-10 15:57:59 -0300

    teste

---
+++ teste/hello.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = pd.read_csv('archive/kc_house_data.csv')
+
+print(data.head())
+
+print(data.shape)
+
+print(data.columns)
+
+print(data[['id','price']].sort_values('price'))
+
+print(data[['id','price']].sort_values('price',ascending=False))
