240a7c6e71267160c8be23dce0aa968060d638e3
= <=>
2025-01-20 10:26:46 -0300

    Acrescentando o uso de SORTED() E ITEMS() no dicionario.

--- useACabeca/capitulo1/dicionario.py
+++ useACabeca/capitulo1/dicionario.py
@@ -18,3 +18,20 @@ for letter in word:
 
 for vowels in dicVowels:
     print(f"vogal:{vowels} : {dicVowels[vowels]}")
+
+# O dicionario é uma estrutura não ordenada (UNSORTED)
+# Para ter um retorno ordenado usar o funcão nativa SORTED()
+
+print("\n Exemplo ordenado [SORTED()]")
+
+for vowels in sorted(dicVowels):
+    print(f"vogal:{vowels} : {dicVowels[vowels]}")
+
+
+print("\n Uso do método Items() do dicionário")
+
+print(f"tipo do retorno do método ITEMS(): {type(dicVowels.items())}")
+print(f"O tipo [dict_items] retorna uma TUPLA quando iterado")
+
+for key,value in sorted(dicVowels.items()):
+    print(f"vogal:{key} : {value}")
