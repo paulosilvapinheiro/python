8509dbe8a75d78dfb40554d1773a3318960c3576
= <=>
2025-01-22 09:57:39 -0300

    Exemplo de tupla

---
+++ useACabeca/capitulo1/tupla.py
@@ -0,0 +1,10 @@
+
+tupla_sample = ()
+print(type(tupla_sample))
+
+# A tupla quando tem apenas 1 item precisa de uma virgula após ele, 
+# para o interpretador a leia como uma tupla e não com o tipo do valor inserido
+tupla_sample = ('paulo',)
+print(type(tupla_sample))
+
+print(tupla_sample)
