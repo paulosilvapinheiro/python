9a27c377d9a407eea04d7a22c5fc8a1c09ed6f7f
= <=>
2025-01-27 08:44:57 -0300

    aplicando ajustes do padrão PEP8

--- useACabeca/capitulo4/myModules/vsearch.py
+++ useACabeca/capitulo4/myModules/vsearch.py
@@ -1,4 +1,4 @@
 
-def search4Vowels(phrase:str, letters:str='aeiou')->set:
-    """Retorna um SET com as letras encontradas na frase fornecida"""   
+def search4Vowels(phrase: str, letters: str='aeiou')->set:
+    """Retorna um SET com as letras encontradas na frase fornecida"""
     return set(letters).intersection(set(phrase))
