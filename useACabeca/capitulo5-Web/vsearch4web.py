dcf3b66446b4c708ce60269de45a666dd7ea2779
paulo <paulo@paulo>
2025-01-28 12:27:03 -0300

    formatado no padrão PEP8

--- useACabeca/capitulo5-Web/vsearch4web.py
+++ useACabeca/capitulo5-Web/vsearch4web.py
@@ -8,15 +8,18 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello workd from Flask'
 
-@app.route('/search4',methods=['GET','POST'])
+
+@app.route('/search4', methods=['GET', 'POST'])
 def do_search() -> str:
     phrase = request.form['Phrase']
     letters = request.form['Letters']
-    return str(search4Vowels(phrase,letters))
+    return str(search4Vowels(phrase, letters))
+
 
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html'
-                           ,the_title = "Welcome to search4Letters onde web!")
+    return render_template('entry.html',
+                           the_title="Welcome to search4Letters onde web!")
+
 
 app.run(debug=True)
