97d88a9ac02adfcffacc6277010297333d822fe7
= <=>
2025-01-31 08:19:56 -0300

    teste para entender o uso dos dunder como o [ __name__ ]

---
+++ useACabeca/capitulo5-Web/dunder_test.py
@@ -0,0 +1,4 @@
+import dunder
+
+# Ao executar perceberá que o valor da variavel __name__ mudou e passou a ser DUNDER, mas quando executada diretamente
+# no programa dunder.y o valor da variavel __name__ é [__main__]
