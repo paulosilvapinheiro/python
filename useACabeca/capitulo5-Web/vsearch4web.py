44a4cee219909df7f24698cc9edbfc84f40806ee
= <=>
2025-01-31 08:00:18 -0300

    Ajustando metodo removido [entry_page]

--- useACabeca/capitulo5-Web/vsearch4web.py
+++ useACabeca/capitulo5-Web/vsearch4web.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from vsearch import search4Vowels
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello() -> str:
-    return 'Hello workd from Flask'
+def hello() -> '302':
+    return redirect('/entry')
 
 
 @app.route('/search4', methods=['GET', 'POST'])
@@ -21,5 +21,12 @@ def do_search() -> str:
                            the_title=title,
                            the_results=results,
                            )
+
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                           the_title="Welcome to search4Letters onde web!")
+
 
 app.run(debug=True)
